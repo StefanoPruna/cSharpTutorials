a8ce6be1d14ab5f39885347882c5b3a57d31d022
StefanoPruna <stefano.pruna@googlemail.com>
2024-09-26 15:32:04 +1000

    first commit

---
+++ OverallReference/OverallReference/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Console.WriteLine("Hello, World!");
+
+namespace Overallreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
+
